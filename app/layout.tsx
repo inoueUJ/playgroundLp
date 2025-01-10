29d81b958a50fae1acf87e09d2a5fa0f7a4db0bd
froide-inoueyuji <yuji-inoue@froide.co.jp>
2025-01-10 20:03:50 +0900

    metaタグ追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import "./globals.css";
 import Footer from "./_components/footer";
 import Header from "./_components/header";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Boss baby - 福岡発3ピースギターロックバンド",
+  description:
+    "平均年齢22歳、福岡発・3ピースギターロックバンド Boss baby の公式サイト",
+  keywords:
+    "Boss baby, ロックバンド, 福岡, ギターロック, しんのすけ, かいり, りょうすけ",
+};
 
 export default function RootLayout({
   children,
@@ -8,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body>
         <Header />
         {children}
